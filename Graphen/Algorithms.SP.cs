89e5ad1926e9c7101654756ad31c545fc0ecd1d8
Exp <17885980+exp111@users.noreply.github.com>
2022-05-03 11:55:14 +0200

    shitty dijkstra

--- Graphen/Algorithms.SP.cs
+++ Graphen/Algorithms.SP.cs
@@ -8,6 +8,41 @@ namespace Graphen
 {
     public static partial class Algorithms
     {
+        public static List<Kante> Dijkstra(this Graph graph, int startID, int endID)
+        {
+            List<Knoten> queue = new();
+            var dist = new double[graph.KnotenAnzahl];
+            Array.Fill(dist, double.MaxValue); // dist = infinite //TODO: maybe null it instead?
+            var start = graph.Knoten[startID];
+            dist[start.ID] = 0;
+            queue.Add(start);
+            var pred = new int[graph.KnotenAnzahl];
+            pred[start.ID] = start.ID;
+            while (queue.Count > 0)
+            {
+                queue.Sort((v, w) =>
+                {
+                    return (int)(dist[v.ID] - dist[w.ID]);
+                });
+                var cur = queue[0];
+                queue.RemoveAt(0);
+                foreach (var edge in cur.Kanten)
+                {
+                    var newCost = dist[cur.ID] + edge.Weight!.Value;
+                    var other = edge.Other(cur);
+                    if (newCost < dist[other.ID])
+                    {
+                        dist[other.ID] = newCost;
+                        pred[other.ID] = cur.ID;
+                        queue.Add(other);
+                    }
+                }
+            }
+
+            //TODO: return graph
+            return new();
+        }
+
         public static bool BellmanFord(this Graph graph)
         {
             var start = graph.Knoten[0];
@@ -20,16 +55,17 @@ namespace Graphen
             // repeat n times
             for (var i = 0; i < graph.KnotenAnzahl - 1; i++)
             {
-                Console.WriteLine($"iteration: {i + 1}");
+                //Console.WriteLine($"iteration: {i + 1}");
                 foreach (var edge in graph.Kanten)
                 {
                     var v = edge.Start.ID;
                     var w = edge.Ende.ID;
                     var c = edge.Weight!.Value;
-                    if (dist[v] + c < dist[w])
+                    var newCost = dist[v] + c;
+                    if (newCost < dist[w])
                     {
-                        Console.WriteLine($"changing {w+1} cause {dist[v]}+{c} < {dist[w]}");
-                        dist[w] = dist[v] + c; //todo: optimize by temp saving
+                        //Console.WriteLine($"changing {w+1} cause {dist[v]}+{c} < {dist[w]}");
+                        dist[w] = newCost;
                         pred[w] = v;
                     }
                 }
